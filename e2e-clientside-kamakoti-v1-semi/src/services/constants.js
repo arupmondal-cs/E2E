8fe803770cbcdb740864a2ad9e6bfa868d4f2e13
Arup Mondal <40867907+arupmondal-cs@users.noreply.github.com>
2021-09-20 23:39:50 +0530

    Kamakoti's First Proposal - Semi-Honest Version

---
+++ e2e-clientside-kamakoti-v1-semi/src/services/constants.js
@@ -0,0 +1,13 @@
+
+const Constants = Object.freeze({
+  MEWO_URL: '',
+  PUBLIC: 'public/',
+  SEARCH: 'search',
+  BASE_URL: 'https://kamakoti-server.herokuapp.com/',
+  USER_TYPE: {
+    SOLO: 'SOLO',
+    ORGANIZATION: 'ORGANIZATION'
+  }
+})
+export default Constants
+
